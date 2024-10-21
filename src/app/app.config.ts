5ee8515be782a693e52a303b238d1e488e852f7b
Moustafa_Ashraf <moustafa.ashraf277@gmail.com>
2024-10-21 23:05:11 +0300

    first commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,30 @@
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
+
+import { routes } from './app.routes';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled',
+      }),
+    ),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    importProvidersFrom([BrowserModule, BrowserAnimationsModule]),
+  ],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EB1F2F",
+      },
+    },
+  },
+  plugins: [],
+}
+
